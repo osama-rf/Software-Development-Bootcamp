5019a75d6aa24d50c05eead3c9d0ddd9a3464af9
n8tu <faisalz.nj@gmail.com>
2022-05-27 21:23:26 +0300

    Added Button Clicker Assignment

---
+++ Day-6/Button Clicker/scripts.js
@@ -0,0 +1,27 @@
+function like(element){
+    var text = element.innerHTML;
+    var number = "";
+    for(var i = 0; i < text.length; i++){
+        if(!isNaN(text[i])){
+            number += text[i];
+        }else if(text[i] = " "){
+            break;
+        }else{
+            continue;
+        }
+    }
+    element.innerHTML = ++number + " " + "likes";
+    alert("Ninja was liked");
+}
+
+function remove(element){
+    element.remove();
+}
+
+function isLoggedIn(element){
+    if(element.innerHTML == "Login"){
+        element.innerHTML = "Logout";
+    }else{
+        element.innerHTML = "Login"
+    }
+}
