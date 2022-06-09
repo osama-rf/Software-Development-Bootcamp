0a08eff51508182e15fe8bdcb2f1cdb34844926a
n8tu <faisalz.nj@gmail.com>
2022-06-09 14:14:42 +0300

    Added CheckBoard assignment

--- Day-13/Checkerboard/checkerboard.py
+++ Day-13/Checkerboard/checkerboard.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html",rows=8 , colums=8)
 
+@app.route('/<int:colums>/<int:rows>')
+def specific_cols_rows(colums,rows):
+    return render_template("index.html",rows=rows, colums=colums)
+
+
+@app.route('/<int:colums>/<int:rows>/<string:color1>/<string:color2>')
+def specific_cols_rows_with_colors(colums,rows,color1,color2):
+    return render_template("index.html",rows=rows, colums=colums,color1=color1,color2=color2)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
